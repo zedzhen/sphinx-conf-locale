e6752c5e08e65adcf4b0dd74ce6ceef9a8422a00
zedzhen <59135268+zedzhen@users.noreply.github.com>
2026-06-03 21:39:07 +0300

    change import alias name

--- src/sphinx_conf_locale/locale.py
+++ src/sphinx_conf_locale/locale.py
@@ -1,13 +1,13 @@
 __all__ = ["_", "_f"]
 
-from sphinx.locale import _ as sphinx__
+from sphinx.locale import _ as sphinx_gettext
 
 from sphinx_conf_locale.lazy_format import LazyFormat
 
 
 def _(message: str) -> str:
-    return LazyFormat(sphinx__(message))
+    return LazyFormat(sphinx_gettext(message))
 
 
 def _f(message: str, *args, **kwargs) -> str:
-    return LazyFormat(sphinx__(message), *args, **kwargs)
+    return LazyFormat(sphinx_gettext(message), *args, **kwargs)
